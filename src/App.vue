4c11770c819e8bbd63c7341b55520dca5901b87f
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-03-14 19:34:53 +0330

    add aside to project

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,42 @@
 <template>
   <div id="app">
     <div class="vh-100 d-flex justify-content-center flex-wrap">
-      <VueNav class="w-100"/>
+      <VueNav class="w-100" />
       <!-- <hr width="0px" class="my-2"> -->
-        <div class="w-100"></div>
-        <Carosel class=" w-75 m-auto"/>
-        <div class="w-100"></div>
-        <div class="w-100"></div>
 
+      <b-alert class="w-100 text-center bg-warning" show dismissible>
+          <h5>سلام به وبسایت من خوش اومدید</h5>
+          <p>من قصد دارم بهترین سایت ممکن رو برای شما عزیزان طراحی و کد نویسی بکنم</p>
+        </b-alert>
+
+      <div class="w-100 row">
+        <Carosel class="col-sm-12 col-lg-9 m-auto px-2" />
+        <Aside class="col-sm-12 col-lg-3 m-auto float-right" />
       </div>
+      <div class="w-100"></div>
+      <div class="w-100"></div>
     </div>
+  </div>
 </template>
 
 <script>
-import VueNav from './components/vue-navbar'
-import Carosel from './components/Carosel'
+import VueNav from "./components/vue-navbar";
+import Carosel from "./components/Carosel";
+import Aside from "./components/Aside";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     VueNav,
-    Carosel
+    Carosel,
+    Aside
   }
-}
+};
 </script>
 
 <style lang="scss">
-
 #app {
-  .vh-100{
+  .vh-100 {
     height: 100%vh;
   }
 }
---
+++ src/components/Aside.vue
@@ -0,0 +1,60 @@
+<template>
+  <b-list-group class="list-items pr-0">
+    <b-list-group-item disabled class="text-center">نوع پروژه های انجام شده</b-list-group-item>
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">Html</b-link>
+      <b-badge variant="primary" pill>14</b-badge>
+    </b-list-group-item>
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">Css</b-link>
+      <b-badge variant="primary" pill>2</b-badge>
+    </b-list-group-item>
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">Javascript</b-link>
+      <b-badge variant="primary" pill>1</b-badge>
+    </b-list-group-item>
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">Bootstrap</b-link>
+      <b-badge variant="primary" pill>14</b-badge>
+    </b-list-group-item>
+
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">php</b-link>
+      <b-badge variant="primary" pill>2</b-badge>
+    </b-list-group-item>
+
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">Vuejs</b-link>
+      <b-badge variant="primary" pill>1</b-badge>
+    </b-list-group-item>
+
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+      <b-link href="#foo">Laravel</b-link>
+      <b-badge variant="primary" pill>1</b-badge>
+    </b-list-group-item>
+  </b-list-group>
+</template>
+
+<script>
+export default {
+  name: "Aside",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.list-items > :hover {
+  background-color: rgba(0, 173, 253, 0.685);
+  color: white;
+}
+.list-items > :hover * {
+  color: white;
+  font-weight: bold;
+}
+</style>
